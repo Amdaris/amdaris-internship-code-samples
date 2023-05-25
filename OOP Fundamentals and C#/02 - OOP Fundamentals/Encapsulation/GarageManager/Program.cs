270e4a724f444e35587a82e0b3c1f9084ec1e29e
Ivan Yorgov <ivan.yorgov@amdaris.com>
2023-05-25 20:43:27 +0300

    Adding code samples for encapsulation

---
+++ OOP Fundamentals and C#/02 - OOP Fundamentals/Encapsulation/GarageManager/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VehicleLibrary;
+
+List<Motorcycle> motorcycles = new List<Motorcycle>();
+motorcycles.Add(new Motorcycle("Ducati", "Ninja"));
+motorcycles.Add(new Motorcycle("Harley-Davidson", "Nightster"));
+
+foreach (var motorcycle in motorcycles)
+{
+    motorcycle.StartEngine();
+}
+
+//List<Car> cars = new List<Car>();
+//cars.Add(new Car("Nissan", "Skyline"));
+//cars.Add(new Car("Subaru", "Impreza"));
+
+//foreach (var car in cars)
+//{
+//    car.StartEnginge();
+//}
---
+++ OOP Fundamentals and C#/02 - OOP Fundamentals/Encapsulation/VehicleLibrary/Motorcycle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleLibrary
+{
+    public class Motorcycle 
+    {
+        public int Speed { get; private set; }
+
+        private string _brand;
+
+        public string _model;
+
+
+        public Motorcycle(string brand, string model) 
+        {
+            _brand = brand;
+            _model = model;
+        }
+
+        public void Accelerate (int amount)
+        {
+            Speed += amount;
+        }
+
+        public void Break()
+        {
+            Speed = 0;
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine($"The {_brand} {_model} starts its engine");
+        }
+
+        public void ShowSpeed()
+        {            
+            Console.WriteLine($"The {_brand} {_model} is currently traveling at {Speed} km/h");
+        }
+
+    }
+}
