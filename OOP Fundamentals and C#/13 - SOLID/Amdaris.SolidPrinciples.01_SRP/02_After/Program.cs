29c14257a86c8f2fb31981fe7eaf73958bb7a1b0
Kiril Domuschiev <kiril.domuschiev@amdaris.com>
2023-05-30 12:16:08 +0300

    Code examples for the SOLID principles.

---
+++ OOP Fundamentals and C#/13 - SOLID/Amdaris.SolidPrinciples.01_SRP/02_After/Program.cs
@@ -0,0 +1,25 @@
+﻿LoggerService loggerService = new LoggerService();
+
+loggerService.LogMessage("Cool message which should be logged!");
+
+NotificationService notificationService = new NotificationService();
+
+notificationService.SendNotification("Amazing notification has been sended!");
+
+#region Classes
+public class LoggerService
+{
+    public void LogMessage(string message)
+    {
+        Console.WriteLine($"Log message: {message}");
+    }
+}
+
+public class NotificationService
+{
+    public void SendNotification(string notificationMessage)
+    {
+        Console.WriteLine($"Notification message: {notificationMessage}");
+    }
+}
+#endregion
---
+++ OOP Fundamentals and C#/13 - SOLID/Amdaris.SolidPrinciples.04_ISP/01_Before/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("ISP overview");
+
+#region Classes
+public interface IAnimal
+{
+    void Eat();
+    void Sleep();
+    void Fly();
+    void Swim();
+}
+
+public class Bird : IAnimal
+{
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+
+    public void Fly()
+    {
+        // Code for flying
+    }
+
+    public void Swim()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Fish : IAnimal
+{
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Swim()
+    {
+        // Code for swimming
+    }
+}
+#endregion
